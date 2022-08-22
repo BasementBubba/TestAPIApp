2cf419a67913551f106e850720f09bb34f01702d
narsel46@gmail.com <narsel46@gmail.com>
2022-08-22 12:43:24 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/DialogsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DialogsController : ControllerBase
+    {
+        List<RGDialogsClients> dialogsClients = new RGDialogsClients().Init();
+
+        [HttpPost("dialogs")]
+        public IEnumerable<Guid> GetDialogs([FromBody] IEnumerable<Guid> clientIDs)
+        {
+            List<Guid> result = new List<Guid>();
+            var dialogs = dialogsClients.GroupBy(d => d.IDRGDialog).ToList();
+            foreach(var dialog in dialogs)
+            {
+                var ids = dialog.Select(d => d.IDClient).ToList();
+                if(ids.Intersect(clientIDs).Count() == clientIDs.Count())
+                {
+                    result.Add(dialog.Key);
+                }
+            }
+            if(result.Count == 0)
+            {
+                result.Add(Guid.Empty);
+            }
+            return result;
+        }
+    }
+}
